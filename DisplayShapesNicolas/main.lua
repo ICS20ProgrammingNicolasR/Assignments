9dcf29caef01b81f383ba31646cc689c36558d85
Nic Riscalas <55556014+s454833@users.noreply.github.com>
2019-09-27 14:28:28 -0400

    stuff

--- DisplayShapesNicolas/main.lua
+++ DisplayShapesNicolas/main.lua
@@ -7,10 +7,13 @@
 display.setDefault("background", 50/255, 200/255, 300/255)
 --create my variables
 local rhombus=display.newPolygon(600, 475, {-200, 300, 200, 300, 100, 0, -300, 0})
-local trapezoid=display.newPolygon(600, 475, {-200,300,200,300,100,0,-600,0})
+local trapezoid=display.newPolygon(600, 300, {-200,300,200,300,100,0,-600,0})
 local triangle=display .newPolygon(600, 475, {-200, 300, 200, 400, 100,0})
 local pentagon=display.newPolygon(600,475, {-200, 300, 200, 300, 100, 0, -150, -100, -300, 0})
-local triangletext
+local triangletext=display.newText("This triangle's area is "..trianglebase*triangleHeight)
+local rhombustext
+local trapezoidtext
+local pentagontext
 local trianglebase=10
 local triangleHeight=15
 local areaoftriangle=trianglebase*triangleHeight
@@ -18,39 +21,15 @@ local areaoftriangle=trianglebase*triangleHeight
 triangle.alpha=0
 trapezoid.alpha=0
 pentagon.alpha=0
---make the rombus fade out
-function Fadeout(event)
+local function fadeinshapes(event)
 	-- body
-	rhombus.alpha=rhombus.alpha-0.001
-end
-
-Runtime:addEventListener("enterFrame", Fadeout)
-
---make the triangle fade in after .5 of a second
-function Fadein(event)
-	-- body
+	rhombus.alpha=rhombus.alpha+0.01
+	trapezoid.alpha=trapezoid.alpha+0.01
 	triangle.alpha=triangle.alpha+0.01
-	timer.performWithDelay( 500, Fadein)
+	pentagon.alpha=pentagon.alpha+0.01
+	triangletext.alpha=triangletext.alpha+0.01
+	rhombustext.alpha=rhombustext.alpha+0.01
+	trapezoidtext.alpha=trapezoidtext.alpha+0.01
+	pentagontext.alpha=pentagontext.alpha+0.01
 end
-Runtime:addEventListener("enterFrame", Fadein) 
-
---make the trangle have a colour and a border
-triangle.strokeWidth=20
-triangle:setStrokeColor(0.5,0,1)
-triangle:setFillColor(0,1,0)
-
---say the area
-triangletext=display.newText("The area of the triangle with the base of  \n"..
-	trianglebase.." cm and a height of  "..triangleHeight.. " cm is "..areaoftriangle.." cm².",570,700, Arial, 50)
-
---make the triangle fade out after 2 seconds
-function FadeoutTriangle(event)
-	-- body
-	triangletext.alpha=1
-	triangle.alpha=triangle.alpha-0.001
-	triangletext.alpha=triangletext.alpha-0.1
-	timer.performWithDelay( 2500, FadeoutTriangle)
-
-end
-
-Runtime:addEventListener("enterFrame", FadeoutTriangle)
+Runtime:addEventListener("enterFrame", fadeinshapes)
