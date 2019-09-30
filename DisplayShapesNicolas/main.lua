d08039b2a2e53c2a78dff8f99694071ada2f0605
Nic Riscalas <55556014+s454833@users.noreply.github.com>
2019-09-30 14:12:25 -0400

    added touch and drag

--- DisplayShapesNicolas/main.lua
+++ DisplayShapesNicolas/main.lua
@@ -10,17 +10,19 @@ local rhombus=display.newPolygon(600, 475, {-200, 300, 200, 300, 100, 0, -300, 0
 local trapezoid=display.newPolygon(600, 300, {-200,300,200,300,100,0,-600,0})
 local triangle=display .newPolygon(600, 475, {-200, 300, 200, 400, 100,0})
 local pentagon=display.newPolygon(600,475, {-200, 300, 200, 300, 100, 0, -150, -100, -300, 0})
-local triangletext=display.newText("This triangle's area is "..trianglebase*triangleHeight)
 local rhombustext
 local trapezoidtext
 local pentagontext
 local trianglebase=10
 local triangleHeight=15
 local areaoftriangle=trianglebase*triangleHeight
+local triangletext
 --set the tansparency to zero 
 triangle.alpha=0
 trapezoid.alpha=0
 pentagon.alpha=0
+--display some newText
+triangletext=display.newText("This triangle's area is "..trianglebase*triangleHeight)
 local function fadeinshapes(event)
 	-- body
 	rhombus.alpha=rhombus.alpha+0.01
