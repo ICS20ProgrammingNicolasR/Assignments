946102a45a846ea860606a33021d586c6e1e4bd7
Nic Riscalas <55556014+s454833@users.noreply.github.com>
2019-10-08 14:37:15 -0400

    started

--- AnimatingImagesNicolasRiscalas/main.lua
+++ AnimatingImagesNicolasRiscalas/main.lua
@@ -1,7 +1,20 @@
 -----------------------------------------------------------------------------------------
---
--- main.lua
---
+--animating images
+-- nic riscalas
+--ics20
+--this programing makes images bounce off the walls of the table and spin and move diagonnally
 -----------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------
+--GLOBAL VARIABLES
+-----------------------------------------------------------------------------------------
+scrollSpeed=3
+-----------------------------------------------------------------------------------------
+-- LOCAL VARIABLES
+-----------------------------------------------------------------------------------------
+local backgroundImage=display.newImageRect("Images/background.jpg", 2048/2, 1536/2)
+backgroundImage.x = display.contentCenterX
+backgroundImage.y=display.contentCenterY
 
--- Your code here
+local lolimage=display.newImageRect("Images/lol image.png", 200, 200)
+lolimage.x=display.contentCenterX
+lolimage.y=display.contentCenterY
