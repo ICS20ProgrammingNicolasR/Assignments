b83e90a03e131f81538e7062b18248e04b9d97ad
Nic Riscalas <55556014+s454833@users.noreply.github.com>
2019-10-10 17:01:16 -0400

    Update main.lua

--- DisplayShapesNicolas/main.lua
+++ DisplayShapesNicolas/main.lua
@@ -2,7 +2,7 @@
 --
 -- main.lua
 --
------------------------------------------------------------------------------------------
+--this program has many shapes that ccan be moved by pressing and holding
 --set the background
 display.setDefault("background", 50/255, 200/255, 300/255)
 --create my variables
@@ -10,12 +10,13 @@ local rhombus=display.newPolygon(700, 500, {100, 100, 500, 100, 400, -50, 0, -50
 local trapezoid=display.newPolygon(700, 200, {-200,300,200,300,100,0,-400,0})
 local triangle=display .newPolygon(200, 600, {-200, 300, 200, 300, 150,0})
 local pentagon=display.newPolygon(300,200, {-200, 300, 200, 300, 50, 0, -150, -100, -300, 0})
-local rhombustext=display.newText("parellelogram\n".. "(click the triangle to continue)",700,600, Arial, 50)
+local rhombustext=display.newText("parellelogram",700,600, Arial, 50)
 local trapezoidtext=display.newText("trapezoid",700,375,Arial,50)
 local pentagontext=display.newText("pentagon", 300,430,Arial,50)
 local areaoftriangle=10*15
 local triangletext=display.newText("The area of this triangle that \n"..
-	"has a height of 10 cm and a base of 15 is 75cm²",500,400,Arial,25)
+	"has a height of 10 cm and a base of 15 is 75cm²(you can move all shapes by \n"..
+	"just pressing and holding",500,400,Arial,25)
 --set the x/y values
 triangle.x=200
 triangle.y=600
@@ -39,6 +40,10 @@ rhombus:setFillColor(200/255,30/255,100/255)
 triangle:setFillColor(40/255,200/255,100/255)
 pentagon:setFillColor(200/255,200/255,50/255)
 trapezoid:setFillColor(100/255,100/255, 255/255)
+pentagontext:setFillColor(200/255, 200/255, 50/255)
+triangletext:setFillColor(40/255,255/255,100/255)
+rhombustext:setFillColor(200/255,30/255,100/255)
+trapezoidtext:setFillColor(100/255,100/255,255/255)
 --create boolean variables for shapes 
 local alreadytouchedtriangle=false
 local alreadytouchedrhombus=false
@@ -54,8 +59,7 @@ Runtime:addEventListener("enterFrame", fadeintriangle)
 local function dragtriangle(touch)
 	
 	if(touch.phase=="began")then
-		alreadytouchedtriangle = true
-		alreadytouchedrhombus = false
+		alreadytouchedtriangle=true
 	end
 	if ((touch.phase=="moved")and(alreadytouchedtriangle==true))then
 		triangle.x=touch.x
@@ -66,17 +70,13 @@ local function dragtriangle(touch)
 		triangle.alpha=triangle.alpha-0.01
 		triangletext.alpha=triangle.alpha
 		timer.performWithDelay(1000,fadeouttriangleandfadeinrhombus)
-		end
-		Runtime:addEventListener("enterFrame",fadeouttriangle)
-	end
-end
+		rhombus.alpha=rhombus.alpha+0.01
+		rhombustext.alpha=rhombustext.alpha+0.01
+ 		end
+ 		Runtime:addEventListener("enterFrame",fadeouttriangle)
+ 	end
+end	
 triangle:addEventListener("touch", dragtriangle)
-
-local function fadeinrhombus(event)
-	rhombus.alpha=rhombus.alpha+0.01
-	rhombustext.alpha=rhombus.alpha
-end
-Runtime:addEventListener("enterFrame",fadeinrhombus)
 local function dragrhombus(touch)
 	if (touch.phase=="began")then
 		alreadytouchedrhombus=true
@@ -88,14 +88,63 @@ local function dragrhombus(touch)
 	if(touch.phase=="ended")then
 		local function fadeoutrhombusandfadeinpentagon(event)
 			rhombus.alpha=rhombus.alpha-0.01
-			rhombustext.alpha=rhombus.alpha
+			rhombustext.alpha=rhombustext.alpha-0.01
 			pentagon.alpha=pentagon.alpha+0.01
-			pentagontext.alpha=pentagon.alpha
+			pentagontext.alpha=pentagontext.alpha+0.01
 			timer.performWithDelay(1000,fadeoutrhombusandfadeinpentagon)
- 			trapezoid.alpha=trapezoid.alpha+0.01
- 			trapezoidtext.alpha=trapezoid.alpha
 		end
 		Runtime:addEventListener("enterFrame",fadeoutrhombusandfadeinpentagon)
 	end
 end
-triangle:addEventListener("touch", dragrhombus)
+rhombus:addEventListener("touch", dragrhombus)
+local function dragpentagon(touch)
+	if (touch.phase=="began")then
+		alreadytouchedpentagon=true
+	end
+	if((touch.phase=="moved")and(alreadytouchedpentagon==true))then
+		pentagon.x=touch.x
+		pentagon.y=touch.y
+	end
+	if(touch.phase=="ended")then
+		local function fadeoutpentagonandfadeintrapezoid(event)
+			pentagon.alpha=pentagon.alpha-0.01
+			pentagontext.alpha=pentagontext.alpha-0.01
+			trapezoid.alpha=trapezoid.alpha+0.01
+			trapezoidtext.alpha=trapezoidtext.alpha+0.01
+			timer.performWithDelay(200,fadeoutpentagonandfadeintrapezoid)
+		end
+		Runtime:addEventListener("enterFrame",fadeoutpentagonandfadeintrapezoid)
+	end
+end
+pentagon:addEventListener("touch", dragpentagon)
+local function dragtrapezoid(touch)
+	if (touch.phase=="began")then
+		alreadytouchedtrapezoid=true
+	end
+	if((touch.phase=="moved")and(alreadytouchedtrapezoid==true))then
+		trapezoid.x=touch.x
+		trapezoid.y=touch.y
+	end
+	if(touch.phase=="ended")then
+		local function fadeinshapes(event)
+			triangle.alpha=1
+			triangletext.alpha=0
+			pentagon.alpha=1
+			pentagontext.alpha=0
+			rhombus.alpha=1
+			rhombustext.alpha=0
+			trapezoidtext.alpha=0
+			triangle.x=200
+			triangle.y=600
+			rhombus.x=700
+			rhombus.y=500
+			trapezoid.x=700
+			trapezoid.y=200
+			pentagon.x=300
+			pentagon.y=200
+			timer.performWithDelay(200,fadeinshapes)			
+		end
+		Runtime:addEventListener("enterFrame",fadeinshapes)
+	end
+end
+trapezoid:addEventListener("touch", dragtrapezoid)
