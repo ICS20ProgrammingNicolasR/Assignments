3b3593f8f431c1bc4c978b5996a5b0bf2bc870fc
Nic Riscalas <55556014+s454833@users.noreply.github.com>
2019-11-18 18:17:39 -0500

    added an image that we don't need

--- AppNicR/mainmenu.lua
+++ AppNicR/mainmenu.lua
@@ -27,3 +27,47 @@ sceneName = "mainmenu"
 
 -- Creating Scene Object
 local scene = composer.newScene( sceneName )
+
+-----------------------------------------------------------------------------------------
+-- LOCAL VARIABLES
+-----------------------------------------------------------------------------------------
+
+local bkg_image
+local playButton
+local creditsButton
+local instructionsButton
+--------------------------------------------------------------------------------------------
+--LOCAL FUNCTIONS
+-----------------------------------------------------------------------------------------------
+-- Creating Transition Function to Credits Page
+local function CreditsTransition( )       
+    composer.gotoScene( "credits_screen", {effect = "zoomOutInRotate", time = 500})
+end 
+
+-----------------------------------------------------------------------------------------
+
+-- Creating Transition to Level1 Screen
+local function Level1ScreenTransition( )
+    composer.gotoScene( "level1_screen", {effect = "slideLeft", time = 1000})
+end    
+-------------------------------------------------------------------------------------------
+--creating transition to instructions screen 
+local function InstructionsTransition( )
+    composer.gotoScene( "instructions", {effect = "zoomOutInRotate", time = 1000})
+end
+
+-----------------------------------------------------------------------------------------
+-- GLOBAL SCENE FUNCTIONS
+-----------------------------------------------------------------------------------------
+function scene:create( event )
+
+	--Creating a group that associates objects with the scene 
+	local sceneGroup = self.view
+    -----------------------------------------------------------------------------------------
+    -- BACKGROUND IMAGE & STATIC OBJECTS
+    -----------------------------------------------------------------------------------------
+    --Insert the BACKGROUND image and position it to the center of the screen
+    bkg_image = display.newImage("Images/backgroundImage.")
+end
+
+return scene
