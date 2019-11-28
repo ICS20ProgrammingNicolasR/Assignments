a4595483646f3ac86c18c9cfbeeb04a114128146
Nic Riscalas <55556014+s454833@users.noreply.github.com>
2019-11-28 16:40:33 -0500

    did more

---
+++ BakerKidsNicRLevel1screen/main.lua
@@ -0,0 +1,18 @@
+-- Title: main.lua
+-- Name: Nic riscalas
+-- Course: ICS2O/3C
+-- This program This calls the splash screen of the app to load itself.
+-----------------------------------------------------------------------------------------
+
+-- Hiding Status Bar
+-- HIDE THE STATUS BAR
+
+-----------------------------------------------------------------------------------------
+
+-- Use composer library
+local composer = require( "composer" )
+
+-----------------------------------------------------------------------------------------
+
+-- Go to the intro screen
+composer.gotoScene( "splashscreen" )
