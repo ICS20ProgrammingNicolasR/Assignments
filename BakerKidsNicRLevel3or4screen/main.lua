4f70f03d8b285f25fe8ec9310ea9c6e6c90cd5f0
Nic Riscalas <55556014+s454833@users.noreply.github.com>
2020-01-07 15:37:20 -0500

    gfd

--- BakerKidsNicRLevel3or4screen/main.lua
+++ BakerKidsNicRLevel3or4screen/main.lua
@@ -15,7 +15,7 @@ local composer = require( "composer" )
 -----------------------------------------------------------------------------------------
 
 -- Go to the intro screen
-composer.gotoScene( "level2_screen" )
+--composer.gotoScene( "level2_screen" )
 --composer.gotoScene( "splashscreen" )
 --composer.gotoScene( "YouLose" )
 --composer.gotoScene( "YouWin" )
